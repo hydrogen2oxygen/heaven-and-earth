3eb150bf7ab884d72427301bbf63b60fa1b9ea7c
hydrogen2oxygen <luxuspeter@googlemail.com>
2020-10-13 14:15:47 +0200

    Initial commit

---
+++ src/test/java/net/hydrogen2oxygen/heavenAndEarth/HeavenAndEarthApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hydrogen2oxygen.heavenAndEarth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavenAndEarthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
